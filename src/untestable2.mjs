516b5f077612b490e3a268a75860321046b247de
frensing <dev@frensing.de>
2024-03-23 13:18:31 +0200

    untestable2

--- src/untestable2.mjs
+++ src/untestable2.mjs
@@ -1,9 +1,13 @@
+// The function diceRoll uses randomness which is hard(/impossible) to test
+// Approach would be to seed (not possible in JS) or run experiments with large sample sizes
 function diceRoll() {
   const min = 1;
   const max = 6;
   return Math.floor(Math.random() * (max + 1 - min) + min);
 }
 
+// This function uses the function above which introduces randomness
+// approach is to extract the function, or give possibility to replace diceRoll function
 export function diceHandValue() {
   const die1 = diceRoll();
   const die2 = diceRoll();
